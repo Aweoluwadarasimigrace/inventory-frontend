8756de3a68b329316a795ceb16e1b76f7cf3e9d1
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-07-08 21:17:17 +0100

    new changes

--- src/(auth)/hooks/useRegister.js
+++ src/(auth)/hooks/useRegister.js
@@ -21,7 +21,9 @@ export const useRegister = () => {
     setisLoading(true);
     e.preventDefault();
     try {
-      const res = await apiClient.post("/auth/register", formData);
+      const res = await apiClient.post("/auth/register", formData,{
+        withCredentials:true
+      });
       console.log(res);
       if (res.data) {
         toast.success("registration successful");
