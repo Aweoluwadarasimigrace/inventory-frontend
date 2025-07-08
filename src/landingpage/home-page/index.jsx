39c70d02d5cd1f1a54153986f6cefd7b20f67d37
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-07-08 17:59:59 +0100

    new files

---
+++ src/landingpage/home-page/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Link to={"/auth"}>register page</Link>
+
+    </div>
+  )
+}
+
+export default HomePage
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import RegisterUser from "./(auth)/register";
+import LandingPage from "./landingpage";
+import HomePage from "./landingpage/home-page";
 
 export const router = createBrowserRouter([
+
+    {
+        path: "/",
+        Component: LandingPage,
+        children:[
+            {index:true, Component:HomePage}
+        ]
+    },
     {
         path: "/auth",
         // Component:
