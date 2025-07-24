b35300eb4f23c76994a18f72352a5c98fec2debf
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-07-24 01:09:33 +0100

    new files

--- src/sharedComponent/profilepage/index.jsx
+++ src/sharedComponent/profilepage/index.jsx
@@ -4,7 +4,10 @@ import AdminProfile from './adminprofilepage'
 import SalesProfile from './salesrepProfilepage'
 
 const ProfilePage = () => {
-    const { user } = useUserStore()
+    const { user, fetchUser } = useUserStore()
+     useEffect(() => {
+    fetchUser(); // call user fetch on page load
+  }, []);
 
     if (!user) { return (<div class="w-10 h-10 border-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"></div>)}
     if(user.role === "admin"){
