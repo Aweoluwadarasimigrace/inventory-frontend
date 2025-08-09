f7b9d88dcbc464a477252013c15bb625e525458d
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-08-09 09:29:56 +0100

    new files

--- src/admin/users/index.jsx
+++ src/admin/users/index.jsx
@@ -18,7 +18,7 @@ const AdminUsersPage = () => {
       <Loader />
     )
   }
-  if (adminUser.length === 0) {
+  if (adminUser?.length === 0) {
     return (
       <div className="flex items-center justify-center h-screen w-full bg-purple-500 text-white">
         <p className="text-lg text-center">No users created yet.</p>
