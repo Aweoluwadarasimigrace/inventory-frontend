5092befc62c581d221fa92549250c71eb56423d6
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-08-18 09:56:29 +0100

    new files

--- src/services/productService.js
+++ src/services/productService.js
@@ -4,6 +4,7 @@ export const fetchallProduct = async(page)=>{
     console.log(page)
   try {
     const response = await apiClient.get(`/product/getallproduct?page=${page}&limit=10`, {withCredentials: true});
+    console.log(response, "response from product service")
     if (!response.ok) { 
        return console.log("Failed to fetch products");
     }
