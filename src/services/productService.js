af16dae38b9c275fa1c3a3a3759536d9bf4f67a3
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-08-19 10:46:47 +0100

    new file

--- src/services/productService.js
+++ src/services/productService.js
@@ -3,7 +3,7 @@ import { apiClient } from "@/lib/client";
 export const fetchallProduct = async(page)=>{
     console.log(page)
   try {
-    const response = await apiClient.get(`/product/getallproduct?page=${page}&limit=10`, {withCredentials: true});
+    const response = await apiClient.get(`/product/getallproduct?page=${page}&limit=10`);
     console.log(response, "response from product service")
     return response.data;
   } catch (error) {
