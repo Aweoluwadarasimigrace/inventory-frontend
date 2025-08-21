483ed667dbf884246bdb1e5a8edef69b61bed1a2
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-08-21 18:08:45 +0100

    new file

--- src/store/getsales.js
+++ src/store/getsales.js
@@ -16,6 +16,7 @@ const useSalesStore = create((set, get)=>({
         set({loading: true, error: null});
         try {
             const data = await fetchAllSales(page);
+            console.log(data)
             set({sales: data.sales, totalPages: data.totalPages, totalsales: data.total, loading: false});
         } catch (error) {
             set({error: error.message, loading: false});
