115640f618b1bb76a0ad2e517579e215736a42b1
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-08-30 21:23:06 +0100

    new file

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -3,6 +3,7 @@ import { apiClient } from "@/lib/client";
 export const fetchDashboardStats = async () => {
   try {
     const response = await apiClient.get('/dashboard/getstats');
+    console.log(response.data, "Fetched dashboard stats");
     return response.data;
   } catch (error) {
     console.error('Error fetching dashboard stats:', error);
