41dab2ded59290df52ebab608d46a3eb614d52b7
Aweoluwadarasimigrace <darasimigrace123@icloud.com>
2025-09-04 09:57:40 +0100

    new fileS

--- src/layout/dashboard/displaytotalcustomer/index.jsx
+++ src/layout/dashboard/displaytotalcustomer/index.jsx
@@ -12,14 +12,14 @@ const DisplayTotalCustomer = () => {
 
     return (
       <div className="flex gap-6">
-  <div className="bg-white rounded-xl shadow p-6 text-center flex-1">
+  <div className="bg-purple-300 rounded-xl shadow p-6 text-center flex-1">
     <h2 className="text-lg font-semibold text-gray-600">👥 Total Customers</h2>
     <p className="text-4xl font-bold text-blue-600 mt-3">
       {customerCount}
     </p>
   </div>
 
-  <div className="bg-white rounded-xl shadow p-6 text-center flex-1">
+  <div className="bg-pink-200 rounded-xl shadow p-6 text-center flex-1">
     <h2 className="text-lg font-semibold text-gray-600">⚠️ Out of Stock Products</h2>
     <p className="text-4xl font-bold text-blue-600 mt-3">
       {count}
